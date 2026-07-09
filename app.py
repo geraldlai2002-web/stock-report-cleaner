638f4afbd954e1b60de7def0a2bb679abba6e769
geraldlai2002-web <geraldlai2002@gmail.com>
2026-07-09 13:47:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Stock Report Cleaner",
+    page_icon="📊"
+)
+
 st.title("📊 Stock Report Cleaner")
 
-st.write("Welcome to my internship project!")
+st.write("Upload your yearly reports (2015-2026)")
+
+uploaded_files = st.file_uploader(
+    "Choose Excel files",
+    type=["xlsx"],
+    accept_multiple_files=True
+)
+
+if st.button("Process"):
+    st.success("Process button clicked!")
