99fa2cccea4c1433510c9cf4d759df61dee528b7
geraldlai2002-web <geraldlai2002@gmail.com>
2026-07-10 13:58:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -286,7 +286,7 @@ if uploaded_files:
             )
 
         st.markdown("---")
-        st.subheader("📊 Complete Report")
+        st.subheader("📊 Preview Report")
 
         st.dataframe(
             final_df,
